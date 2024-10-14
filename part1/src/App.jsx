71376ff1a8bbfbe7c6f5620169335f996dbf1ad3
odin <git config --global user.email 123456789+odin@users.noreply.github.com>
2024-10-14 21:11:16 +0400

    changed app to display friends

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -1,35 +1,17 @@
-const Hello = (props) => {
-  console.log(props);
-  return (
-    <div id={props.myid}>
-      <p>
-        Hello {props.name}, you are {props.age} years old
-      </p>
-    </div>
-  );
-};
-
-const Footer = () => {
-  return (
-    <>
-      greeting app created by{" "}
-      <a href="https://github.com/leonti98" target="blank">
-        Levan Vachnadze
-      </a>
-    </>
-  );
-};
-
 const App = () => {
-  const name = "Peter";
-  const age = 10;
+  const friends = [
+    { name: 'Peter', age: 4 },
+    { name: 'Maya', age: 10 },
+  ];
 
   return (
-    <div id="first">
-      <h1>Greetings</h1>
-      <Hello name="Maya" age={26 + 10} myid={1} />
-      <Hello name={name} age={age} myid={2} />
-      <Footer />
+    <div>
+      <p>
+        {friends[0].name} {friends[0].age}
+      </p>
+      <p>
+        {friends[1].name} {friends[1].age}
+      </p>
     </div>
   );
 };
