9fcbf94a07b3bddd0bcd8dc0fcf97c71c17a42d6
odin <git config --global user.email 123456789+odin@users.noreply.github.com>
2024-10-17 01:03:35 +0400

    conditional rendering

--- part1/learning_materials/1d_more_complex_state_and_debugging/learning_materials/complex_state/src/App.jsx
+++ part1/learning_materials/1d_more_complex_state_and_debugging/learning_materials/complex_state/src/App.jsx
@@ -1,28 +1,27 @@
 import { useState } from 'react';
 
+const History = (props) => {
+  if (props.allClicks.length === 0) {
+    return <div>the app is used by pressing the buttons</div>;
+  }
+  return <div>button pressed history: {props.allClicks.join(' ')}</div>;
+};
+
 const App = () => {
   const [left, setLeft] = useState(0);
   const [right, setRight] = useState(0);
   const [allClicks, setAll] = useState([]);
 
-  const [total, setTotal] = useState(0);
-
   const handleLeftClick = () => {
     setAll(allClicks.concat('L'));
-    console.log('left before', left);
     const updateLeft = left + 1;
     setLeft(updateLeft);
-    console.log('left after', left);
-    console.log('update left', updateLeft);
-
-    setTotal(updateLeft + right);
   };
 
   const handleRightClick = () => {
     setAll(allClicks.concat('R'));
     const updatedRight = right + 1;
     setRight(updatedRight);
-    setTotal(left + updatedRight);
   };
 
   return (
@@ -31,9 +30,7 @@ const App = () => {
       <button onClick={handleLeftClick}>left</button>
       <button onClick={handleRightClick}>right</button>
       {right}
-      <p>{allClicks.join(' ')}</p>
-
-      <p>total {total}</p>
+      <History allClicks={allClicks} />
     </div>
   );
 };
