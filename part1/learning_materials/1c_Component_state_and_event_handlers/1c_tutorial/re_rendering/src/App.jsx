2d51a66fd23296adbfbd1aa973f7fa3ad331ff2e
odin <git config --global user.email 123456789+odin@users.noreply.github.com>
2024-10-16 21:31:08 +0400

    Destruting component

--- part1/learning_materials/1c_Component_state_and_event_handlers/1c_tutorial/re_rendering/src/App.jsx
+++ part1/learning_materials/1c_Component_state_and_event_handlers/1c_tutorial/re_rendering/src/App.jsx
@@ -1,21 +1,35 @@
 import { useState } from 'react';
 
+const Display = ({ counter }) => <div>{counter}</div>;
+
+const Button = ({ onSmash, text }) => <button onClick={onSmash}>{text}</button>;
+
 const App = () => {
   const [counter, setCounter] = useState(0);
 
-  const increaseByOne = () => setCounter(counter + 1);
+  console.log('rendering with counter value', counter);
 
-  const setZero = () => setCounter(0);
+  const increaseByOne = () => {
+    console.log('increasing, value before', counter);
+    setCounter(counter + 1);
+  };
+
+  const decreaseByOne = () => {
+    console.log('decreasing, value before', counter);
+    setCounter(counter - 1);
+  };
+
+  const setToZero = () => {
+    console.log('resetting to zero, value before', counter);
+    setCounter(0);
+  };
 
   return (
     <div>
-      <div>
-        <div>{counter}</div>
-      </div>
-      <button onClick={increaseByOne}>plus</button>
-      <div>
-        <button onClick={setZero}>reset</button>
-      </div>
+      <Display counter={counter} />
+      <Button onSmash={increaseByOne} text="plus" />
+      <Button onSmash={setToZero} text="zero" />
+      <Button onSmash={decreaseByOne} text="minus" />
     </div>
   );
 };
