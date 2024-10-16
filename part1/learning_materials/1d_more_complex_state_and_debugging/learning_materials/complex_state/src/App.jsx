77df74cc19368c63d948c4487ea1dd8d9c75efb3
odin <git config --global user.email 123456789+odin@users.noreply.github.com>
2024-10-17 00:44:45 +0400

    use of the state is asynchronous

--- part1/learning_materials/1d_more_complex_state_and_debugging/learning_materials/complex_state/src/App.jsx
+++ part1/learning_materials/1d_more_complex_state_and_debugging/learning_materials/complex_state/src/App.jsx
@@ -3,33 +3,33 @@ import { useState } from 'react';
 const App = () => {
   const [left, setLeft] = useState(0);
   const [right, setRight] = useState(0);
-
   const [allClicks, setAll] = useState([]);
 
+  const [total, setTotal] = useState(0);
+
   const handleLeftClick = () => {
-    console.log(allClicks);
     setAll(allClicks.concat('L'));
-    console.log(allClicks);
     setLeft(left + 1);
+
+    setTotal(left + right);
   };
 
   const handleRightClick = () => {
-    console.log(allClicks, 'before');
     setAll(allClicks.concat('R'));
-    console.log(allClicks, 'after');
     setRight(right + 1);
+
+    setTotal(left + right);
   };
-  console.log('====================================');
-  console.log(allClicks);
-  console.log('====================================');
+
   return (
     <div>
       {left}
       <button onClick={handleLeftClick}>left</button>
       <button onClick={handleRightClick}>right</button>
       {right}
-
       <p>{allClicks.join(' ')}</p>
+
+      <p>total {total}</p>
     </div>
   );
 };
