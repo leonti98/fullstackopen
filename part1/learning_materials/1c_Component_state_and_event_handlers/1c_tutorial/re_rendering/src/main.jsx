db2a7cd8fcaf1bf0482b30d30496d4a904e44c2d
odin <git config --global user.email 123456789+odin@users.noreply.github.com>
2024-10-16 16:06:04 +0400

    interval refresh

--- part1/learning_materials/1c_Component_state_and_event_handlers/1c_tutorial/re_rendering/src/main.jsx
+++ part1/learning_materials/1c_Component_state_and_event_handlers/1c_tutorial/re_rendering/src/main.jsx
@@ -1,10 +1,16 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import ReactDOM from 'react-dom/client';
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+import App from './App';
+
+let counter = 1;
+
+const refresh = () => {
+  ReactDOM.createRoot(document.getElementById('root')).render(
+    <App counter={counter} />
+  );
+};
+
+setInterval(() => {
+  refresh();
+  counter += 1;
+}, 1000);
