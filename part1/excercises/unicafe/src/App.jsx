937923d45792b787939d7e2ca6710411336e9979
odin <git config --global user.email 123456789+odin@users.noreply.github.com>
2024-10-21 02:05:20 +0400

    Finished unicafe step 3

--- part1/excercises/unicafe/src/App.jsx
+++ part1/excercises/unicafe/src/App.jsx
@@ -4,6 +4,21 @@ const Button = (props) => {
   return <button onClick={props.clickhandler}>{props.text}</button>;
 };
 
+const Statistics = (props) => {
+  const { good, neutral, bad } = props;
+  return (
+    <div>
+      <h2>Statistics </h2>
+      <p>good {good}</p>
+      <p>neutral {neutral}</p>
+      <p>bad {bad}</p>
+      <p>all {good + bad + neutral}</p>
+      <p>average {(good - bad) / (good + bad + neutral)}</p>
+      <p>positive {good / (good + bad + neutral)} %</p>
+    </div>
+  );
+};
+
 const App = () => {
   // save clicks of each button to its own state
   const [good, setGood] = useState(0);
@@ -23,7 +38,7 @@ const App = () => {
   };
 
   return (
-    <>
+    <div>
       <h1>Give feedback</h1>
       <Button clickhandler={() => setGoodVotes(good + 1)} text="good" />
       <Button
@@ -31,14 +46,8 @@ const App = () => {
         text="neutral"
       />
       <Button clickhandler={() => setBadVotes(bad + 1)} text="bad" />
-      <h2>Statistics </h2>
-      <p>good {good}</p>
-      <p>neutral {neutral}</p>
-      <p>bad {bad}</p>
-      <p>all {good + bad + neutral}</p>
-      <p>average {(good - bad) / (good + bad + neutral)}</p>
-      <p>positive {good / (good + bad + neutral)} %</p>
-    </>
+      <Statistics good={good} neutral={neutral} bad={bad} />
+    </div>
   );
 };
 
