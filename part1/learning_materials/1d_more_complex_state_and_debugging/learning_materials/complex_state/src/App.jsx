79d60b32d9f015ef20b562c7ac6b4c3223d09b8e
odin <git config --global user.email 123456789+odin@users.noreply.github.com>
2024-10-21 00:12:10 +0400

    Set value excercise

--- part1/learning_materials/1d_more_complex_state_and_debugging/learning_materials/complex_state/src/App.jsx
+++ part1/learning_materials/1d_more_complex_state_and_debugging/learning_materials/complex_state/src/App.jsx
@@ -7,6 +7,10 @@ const History = (props) => {
   return <div>button pressed history: {props.allClicks.join(' ')}</div>;
 };
 
+const hello = (who) => () => {
+  console.log('hello', who);
+};
+
 const Button = (props) => {
   console.log('props value is ', props);
   const { handleClick, text } = props;
@@ -14,33 +18,17 @@ const Button = (props) => {
 };
 
 const App = () => {
-  const [left, setLeft] = useState(0);
-  const [right, setRight] = useState(0);
-  const [allClicks, setAll] = useState([]);
-
-  const handleLeftClick = () => {
-    setAll(allClicks.concat('L'));
-    const updateLeft = left + 1;
-    setLeft(updateLeft);
+  const [value, setValue] = useState(10);
+  const setToValue = (newValue) => () => {
+    console.log('value now', newValue); // print the new value to console
+    setValue(newValue);
   };
-
-  const handleRightClick = () => {
-    setAll(allClicks.concat('R'));
-    const updatedRight = right + 1;
-    setRight(updatedRight);
-  };
-  debugger;
   return (
     <div>
-      {left}
-      <Button handleClick={handleLeftClick} text="left">
-        left
-      </Button>
-      <Button handleClick={handleRightClick} text="right">
-        right
-      </Button>
-      {right}
-      <History allClicks={allClicks} />
+      {value}
+      <button onClick={setToValue(1000)}>thousand</button>{' '}
+      <button onClick={setToValue(0)}>reset</button>{' '}
+      <button onClick={setToValue(value + 1)}>increment</button>
     </div>
   );
 };
