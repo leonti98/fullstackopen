228a2faa672491e49dcdbc22cda56fc6b5c8288d
odin <git config --global user.email 123456789+odin@users.noreply.github.com>
2024-10-21 00:46:40 +0400

    Return function from function

--- part1/learning_materials/1d_more_complex_state_and_debugging/learning_materials/complex_state/src/App.jsx
+++ part1/learning_materials/1d_more_complex_state_and_debugging/learning_materials/complex_state/src/App.jsx
@@ -11,24 +11,24 @@ const hello = (who) => () => {
   console.log('hello', who);
 };
 
-const Button = (props) => {
-  console.log('props value is ', props);
-  const { handleClick, text } = props;
-  return <button onClick={handleClick}>{text}</button>;
-};
+const Button = (props) => (
+  <button onClick={props.handleClick}>{props.text}</button>
+);
 
 const App = () => {
   const [value, setValue] = useState(10);
-  const setToValue = (newValue) => () => {
-    console.log('value now', newValue); // print the new value to console
+
+  const setToValue = (newValue) => {
+    console.log('value now', newValue);
     setValue(newValue);
   };
+
   return (
     <div>
       {value}
-      <button onClick={setToValue(1000)}>thousand</button>{' '}
-      <button onClick={setToValue(0)}>reset</button>{' '}
-      <button onClick={setToValue(value + 1)}>increment</button>
+      <Button handleClick={() => setToValue(1000)} text="thousand" />
+      <Button handleClick={() => setToValue(0)} text="reset" />
+      <Button handleClick={() => setToValue(value + 1)} text="increment" />
     </div>
   );
 };
