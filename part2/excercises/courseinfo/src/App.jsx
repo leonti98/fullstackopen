784ccdcfc7217b22c96b549fe79265196b6cfae9
odin <git config --global user.email 123456789+odin@users.noreply.github.com>
2024-10-23 02:03:55 +0400

    Declared course as separate module and imported

--- part2/excercises/courseinfo/src/App.jsx
+++ part2/excercises/courseinfo/src/App.jsx
@@ -1,29 +1,4 @@
-const Header = ({ name }) => <h2>{name}</h2>;
-
-const Total = ({ parts }) => (
-  <p>
-    Total of {parts.reduce((total, part) => total + part.exercises, 0)}{' '}
-    exercises
-  </p>
-);
-
-const Part = ({ part }) => (
-  <p>
-    {part.name} {part.exercises}
-  </p>
-);
-
-const Content = ({ parts }) => {
-  return parts.map((part, i) => <Part key={i} part={part}></Part>);
-};
-
-const Course = ({ course }) => (
-  <div>
-    <Header key={course.id} name={course.name} />
-    <Content parts={course.parts} />
-    <Total parts={course.parts} />
-  </div>
-);
+import Course from './components/Course';
 
 const App = () => {
   const courses = [
