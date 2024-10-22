10723733dfc4b084067e45e92be83f8a682d1956
odin <git config --global user.email 123456789+odin@users.noreply.github.com>
2024-10-22 20:27:27 +0400

    Assigning index as key while using map

--- part2/class_materials/2a_rendering_collection_modules/src/App.jsx
+++ part2/class_materials/2a_rendering_collection_modules/src/App.jsx
@@ -1,13 +1,14 @@
 const App = (props) => {
   const { notes } = props;
   console.log(notes);
-
+  const result = notes.map((note) => <li key={note.id}>note.content</li>);
+  console.log(result);
   return (
     <div>
       <h1>Notes</h1>
       <ul>
-        {notes.map((note) => (
-          <li key={note.id}>{note.content}</li>
+        {notes.map((note, i) => (
+          <li key={i}>{note.content}</li>
         ))}
       </ul>
     </div>
