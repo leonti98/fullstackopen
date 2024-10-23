98ed3499994f833e8a360ea933a39e9f48810721
odin <git config --global user.email 123456789+odin@users.noreply.github.com>
2024-10-23 21:27:55 +0400

    Working search functionality

--- part2/excercises/phonebook/src/App.jsx
+++ part2/excercises/phonebook/src/App.jsx
@@ -6,13 +6,42 @@ const Person = ({ name, phone }) => (
   </p>
 );
 
+const Filter = ({ persons, searchField }) => {
+  console.log(persons);
+  console.log(searchField);
+  if (searchField === '') {
+    return '';
+  }
+  const filteredPersons = persons
+    .filter((person) =>
+      person.name.toLowerCase().includes(searchField.toLowerCase())
+    )
+    .map((person, i) => {
+      console.log(person);
+      return (
+        <Person
+          name={person.name}
+          phone={person.phone}
+          key={person.phone.id}
+        ></Person>
+      );
+    });
+  console.log(filteredPersons);
+
+  return <>{filteredPersons}</>;
+};
+
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', phone: '123' },
+    { name: 'Arto Hellas', phone: '040-123456', id: 1 },
+    { name: 'Ada Lovelace', phone: '39-44-5323523', id: 2 },
+    { name: 'Dan Abramov', phone: '12-43-234345', id: 3 },
+    { name: 'Mary Poppendieck', phone: '39-23-6423122', id: 4 },
   ]);
   const [formData, setFormData] = useState({ personName: '', phone: '' });
   const [personAlreadyExists, setPersonAlreadyExists] = useState(false);
   const [lastEntry, setLastEntry] = useState('');
+  const [searchField, setSearchField] = useState('');
 
   const checkIfExists = (newEntry) => {
     const exists = persons.filter(
@@ -51,9 +80,27 @@ const App = () => {
     setFormData({ ...formData, [event.target.name]: event.target.value });
   };
 
+  const searchPerson = (event) => {
+    setSearchField(event.target.value);
+  };
+
   return (
     <div>
       <h2>Phonebook</h2>
+
+      <form>
+        <div>
+          filter show with
+          <input
+            type="text"
+            name="search"
+            value={searchField}
+            onChange={searchPerson}
+          />
+        </div>
+      </form>
+      <Filter persons={persons} searchField={searchField} />
+      <h2>add a new</h2>
       <form onSubmit={addPerson}>
         <div>
           name:{' '}
@@ -76,11 +123,9 @@ const App = () => {
         </div>
       </form>
       <h2>Numbers</h2>
-
       {persons.map((parson, i) => (
         <Person name={parson.name} phone={parson.phone} key={i}></Person>
       ))}
-
       {personAlreadyExists ? (
         <p>{lastEntry} is already added to phonebook</p>
       ) : (
