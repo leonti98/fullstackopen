e1115f5019d177971e7911fa8ff42751bf01aafe
odin <git config --global user.email 123456789+odin@users.noreply.github.com>
2024-10-23 15:46:48 +0400

    Step 1 finished. adding new person to phonebook

--- part2/excercises/phonebook/src/App.jsx
+++ part2/excercises/phonebook/src/App.jsx
@@ -1,23 +1,41 @@
 import { useState } from 'react';
 
+const Person = ({ name }) => <li>{name}</li>;
+
 const App = () => {
   const [persons, setPersons] = useState([{ name: 'Arto Hellas' }]);
   const [newName, setNewName] = useState('');
 
+  const addPerson = (event) => {
+    event.preventDefault();
+    const newEntry = {
+      name: newName,
+    };
+    setPersons(persons.concat(newEntry));
+    setNewName('');
+  };
+
+  const handleFormChange = (event) => {
+    console.log(event.target.value);
+    setNewName(event.target.value);
+  };
   return (
     <div>
       <h2>Phonebook</h2>
-      <div>debug: {newName}</div>
-      <form>
+      <form onSubmit={addPerson}>
         <div>
-          name: <input />
+          name: <input onChange={handleFormChange} value={newName} />
         </div>
         <div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
-      ...
+      <ul>
+        {persons.map((parson, i) => (
+          <Person name={parson.name} key={i}></Person>
+        ))}
+      </ul>
     </div>
   );
 };
