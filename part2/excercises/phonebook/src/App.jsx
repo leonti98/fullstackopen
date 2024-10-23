c784df1194bd98b599189d92dfb30297e220724d
odin <git config --global user.email 123456789+odin@users.noreply.github.com>
2024-10-23 20:12:07 +0400

    Step 3 of phonebook finished

--- part2/excercises/phonebook/src/App.jsx
+++ part2/excercises/phonebook/src/App.jsx
@@ -1,15 +1,24 @@
 import { useState } from 'react';
 
-const Person = ({ name }) => <p>{name}</p>;
+const Person = ({ name, phone }) => (
+  <p>
+    {name} {phone}
+  </p>
+);
 
 const App = () => {
-  const [persons, setPersons] = useState([{ name: 'Arto Hellas' }]);
-  const [newName, setNewName] = useState('');
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas', phone: '123' },
+  ]);
+  const [formData, setFormData] = useState({ personName: '', phone: '' });
   const [personAlreadyExists, setPersonAlreadyExists] = useState(false);
   const [lastEntry, setLastEntry] = useState('');
 
   const checkIfExists = (newEntry) => {
-    const exists = persons.filter((person) => person.name === newEntry.name);
+    const exists = persons.filter(
+      (person) =>
+        person.name === newEntry.name && person.phone === newEntry.phone
+    );
     const currentPersonExists = exists.length > 0 ? true : false;
     setPersonAlreadyExists(currentPersonExists);
     console.log('currentPersonExists', currentPersonExists);
@@ -19,22 +28,27 @@ const App = () => {
   const addPerson = (event) => {
     event.preventDefault();
     const newEntry = {
-      name: newName,
+      name: formData.personName,
+      phone: formData.phone,
     };
 
     if (checkIfExists(newEntry)) {
       console.log('exists');
-      setLastEntry(newName);
+      setLastEntry(
+        `person ${formData.personName} with phone ${formData.phone}`
+      );
     } else {
       console.log('does not exists');
-
       setPersons(persons.concat(newEntry));
-      setNewName('');
+      setFormData({ personName: '', phone: '' });
+      setLastEntry(
+        `person ${formData.personName} with phone ${formData.phone}`
+      );
     }
   };
 
   const handleFormChange = (event) => {
-    setNewName(event.target.value);
+    setFormData({ ...formData, [event.target.name]: event.target.value });
   };
 
   return (
@@ -42,7 +56,20 @@ const App = () => {
       <h2>Phonebook</h2>
       <form onSubmit={addPerson}>
         <div>
-          name: <input onChange={handleFormChange} value={newName} />
+          name:{' '}
+          <input
+            name="personName"
+            onChange={handleFormChange}
+            value={formData.personName}
+          />
+        </div>
+        <div>
+          phone:{' '}
+          <input
+            name="phone"
+            onChange={handleFormChange}
+            value={formData.phone}
+          />
         </div>
         <div>
           <button type="submit">add</button>
@@ -51,13 +78,15 @@ const App = () => {
       <h2>Numbers</h2>
 
       {persons.map((parson, i) => (
-        <Person name={parson.name} key={i}></Person>
+        <Person name={parson.name} phone={parson.phone} key={i}></Person>
       ))}
 
       {personAlreadyExists ? (
         <p>{lastEntry} is already added to phonebook</p>
       ) : (
-        ''
+        <>
+          <p>{lastEntry} has been added to phonebook</p>
+        </>
       )}
     </div>
   );
