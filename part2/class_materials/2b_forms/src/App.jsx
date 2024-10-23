fce5010fecf0bf7acee80108c7ba7083f0ea5899
odin <git config --global user.email 123456789+odin@users.noreply.github.com>
2024-10-23 14:54:26 +0400

    Adding note completed

--- part2/class_materials/2b_forms/src/App.jsx
+++ part2/class_materials/2b_forms/src/App.jsx
@@ -3,6 +3,24 @@ import Note from './components/Note';
 
 const App = (props) => {
   const [notes, setNotes] = useState(props.notes);
+  console.log(notes);
+  const [newNote, setNewNote] = useState('a new note...');
+
+  const addNote = (event) => {
+    event.preventDefault();
+    const noteObject = {
+      content: newNote,
+      important: Math.random() < 0.5,
+      id: String(notes, length + 1),
+    };
+    setNotes(notes.concat(noteObject));
+    setNewNote('');
+  };
+
+  const handleNoteChange = (event) => {
+    console.log(event.target.value);
+    setNewNote(event.target.value);
+  };
   return (
     <div>
       <h1>Notes</h1>
@@ -11,6 +29,10 @@ const App = (props) => {
           <Note key={note.id} note={note} />
         ))}
       </ul>
+      <form onSubmit={addNote}>
+        <input value={newNote} onChange={handleNoteChange} />
+        <button type="submit">save</button>
+      </form>
     </div>
   );
 };
