1bc471cd511341ce85773e320dc6b151de6160d0
Levan Vachnadze <lvachnadze1@gmail.com>
2024-10-25 02:24:40 +0400

    Finished 2c leanrning material

--- part2/class_materials/2c_getting_data_from_server/src/App.jsx
+++ part2/class_materials/2c_getting_data_from_server/src/App.jsx
@@ -1,10 +1,24 @@
-import { useState } from 'react';
-import Note from './components/Note';
+import { useState, useEffect } from "react";
+import Note from "./components/Note";
+import axios from "axios";
 
-const App = (props) => {
-  const [notes, setNotes] = useState(props.notes);
-  const [newNote, setNewNote] = useState('a new note...');
+const App = () => {
+  const [notes, setNotes] = useState([]);
+  const [newNote, setNewNote] = useState("a new note...");
   const [showAll, setShowAll] = useState(true);
+
+  const hook = () => {
+    console.log("effect");
+    axios.get("http://localhost:3001/notes").then((response) => {
+      console.log("promise fulfilled");
+      setNotes(response.data);
+    });
+  };
+
+  useEffect(hook, []);
+
+  console.log("render", notes.length, "notes");
+  console.log(notes);
 
   const addNote = (event) => {
     event.preventDefault();
@@ -14,7 +28,7 @@ const App = (props) => {
       id: String(notes.length + 1),
     };
     setNotes(notes.concat(noteObject));
-    setNewNote('');
+    setNewNote("");
   };
 
   const handleNoteChange = (event) => {
@@ -31,7 +45,7 @@ const App = (props) => {
       <h1>Notes</h1>
       <div>
         <button onClick={() => setShowAll(!showAll)}>
-          show {showAll ? 'important' : 'all'}
+          show {showAll ? "important" : "all"}
         </button>
       </div>
       <ul>
