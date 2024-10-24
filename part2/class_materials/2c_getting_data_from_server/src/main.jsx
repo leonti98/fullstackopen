76e34f4a607084a44dca8210569c1eb998e8c96f
odin <git config --global user.email 123456789+odin@users.noreply.github.com>
2024-10-24 21:14:15 +0400

    Created promises

--- part2/class_materials/2c_getting_data_from_server/src/main.jsx
+++ part2/class_materials/2c_getting_data_from_server/src/main.jsx
@@ -1,4 +1,14 @@
 import ReactDOM from 'react-dom/client';
+import axios from 'axios';
+
+const promise = axios.get('http://localhost:3001/notes');
+console.log(promise);
+
+const promise2 = axios.get('http://localhost:3001/foobar');
+console.log(promise2);
+promise.then((response) => {
+  console.log(response);
+});
 
 import App from './App';
 
