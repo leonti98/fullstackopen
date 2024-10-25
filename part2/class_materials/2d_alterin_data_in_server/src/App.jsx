d97afbc8be01f829ddd9794cbdcf8d6054a1e35b
odin <git config --global user.email 123456789+odin@users.noreply.github.com>
2024-10-26 03:20:46 +0400

    Using put method to update database

--- part2/class_materials/2d_alterin_data_in_server/src/App.jsx
+++ part2/class_materials/2d_alterin_data_in_server/src/App.jsx
@@ -7,6 +7,16 @@ const App = () => {
   const [newNote, setNewNote] = useState('');
   const [showAll, setShowAll] = useState(false);
 
+  const toggleImportanceOf = (id) => {
+    const url = `http://localhost:3001/notes/${id}`;
+    const note = notes.find((n) => n.id === id);
+    const changedNote = { ...note, important: !note.important };
+
+    axios.put(url, changedNote).then((response) => {
+      setNotes(notes.map((n) => (n.id === id ? response.data : n)));
+    });
+  };
+
   useEffect(() => {
     axios.get('http://localhost:3001/notes').then((response) => {
       setNotes(response.data);
@@ -42,7 +52,11 @@ const App = () => {
       </div>
       <ul>
         {notesToShow.map((note) => (
-          <Note key={note.id} note={note} />
+          <Note
+            key={note.id}
+            note={note}
+            toggleImportance={() => toggleImportanceOf(note.id)}
+          />
         ))}
       </ul>
       <form onSubmit={addNote}>
