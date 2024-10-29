161f1a76603e00d7c51eac5c8bbb54b7f063164b
Levan Vachnadze <lvachnadze1@gmail.com>
2024-10-29 19:44:55 +0400

    More pleasant version

--- part2/class_materials/2d_alterin_data_in_server/src/App.jsx
+++ part2/class_materials/2d_alterin_data_in_server/src/App.jsx
@@ -1,27 +1,26 @@
 import { useState, useEffect } from "react";
 import axios from "axios";
 import Note from "./components/Note";
+import noteService from "./services/notes";
 
 const App = () => {
   const [notes, setNotes] = useState([]);
   const [newNote, setNewNote] = useState("");
   const [showAll, setShowAll] = useState(false);
 
+  useEffect(() => {
+    noteService.getAll().then((initialNotes) => setNotes(initialNotes));
+  });
+
   const toggleImportanceOf = (id) => {
     const url = `http://localhost:3001/notes/${id}`;
     const note = notes.find((n) => n.id === id);
     const changedNote = { ...note, important: !note.important };
 
-    axios.put(url, changedNote).then((response) => {
-      setNotes(notes.map((n) => (n.id === id ? response.data : n)));
+    noteService.update(id, changedNote).then((returnedNote) => {
+      setNotes(notes.map((note) => (note.id === id ? returnedNote : note)));
     });
   };
-
-  useEffect(() => {
-    axios.get("http://localhost:3001/notes").then((response) => {
-      setNotes(response.data);
-    });
-  }, []);
 
   const addNote = (event) => {
     event.preventDefault();
@@ -30,8 +29,8 @@ const App = () => {
       important: Math.random() > 0.5,
     };
 
-    axios.post("http://localhost:3001/notes", noteObject).then((response) => {
-      setNotes(notes.concat(response.data));
+    noteService.create(noteObject).then((returnedNote) => {
+      setNotes(notes.concat(returnedNote));
       setNewNote("");
     });
   };
