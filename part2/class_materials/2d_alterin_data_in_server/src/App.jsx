b93563b4b32a4d38ea2148663ca90b99a142750e
Levan Vachnadze <lvachnadze1@gmail.com>
2024-10-29 20:14:08 +0400

    Catching errors / exceptions from promise

--- part2/class_materials/2d_alterin_data_in_server/src/App.jsx
+++ part2/class_materials/2d_alterin_data_in_server/src/App.jsx
@@ -6,20 +6,26 @@ import noteService from "./services/notes";
 const App = () => {
   const [notes, setNotes] = useState([]);
   const [newNote, setNewNote] = useState("");
-  const [showAll, setShowAll] = useState(false);
+  const [showAll, setShowAll] = useState(true);
 
   useEffect(() => {
     noteService.getAll().then((initialNotes) => setNotes(initialNotes));
-  });
+  }, []);
 
   const toggleImportanceOf = (id) => {
     const url = `http://localhost:3001/notes/${id}`;
     const note = notes.find((n) => n.id === id);
     const changedNote = { ...note, important: !note.important };
 
-    noteService.update(id, changedNote).then((returnedNote) => {
-      setNotes(notes.map((note) => (note.id === id ? returnedNote : note)));
-    });
+    noteService
+      .update(id, changedNote)
+      .then((returnedNote) => {
+        setNotes(notes.map((note) => (note.id === id ? returnedNote : note)));
+      })
+      .catch((error) => {
+        alert(`the note '${note.content}' was already deleted from server`);
+        setNotes(notes.filter((n) => n.id !== id));
+      });
   };
 
   const addNote = (event) => {
