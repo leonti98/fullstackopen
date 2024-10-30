124e92fa464d015e83b83da95e84068fa4dc7fd0
odin <git config --global user.email 123456789+odin@users.noreply.github.com>
2024-10-30 23:55:01 +0400

    Switched to github version to follow excercises

--- part2/class_materials/2e_adding_styles/src/App.jsx
+++ part2/class_materials/2e_adding_styles/src/App.jsx
@@ -1,14 +1,13 @@
 import { useState, useEffect } from 'react';
 import Note from './components/Note';
-import noteService from './services/notes';
-import './index.css';
 import Notification from './components/Notification';
+import noteService from './services/notes';
 
 const App = () => {
   const [notes, setNotes] = useState([]);
   const [newNote, setNewNote] = useState('');
-  const [showAll, setShowAll] = useState(false);
-  const [errorMessage, setErrorMessage] = useState('some error happened...');
+  const [showAll, setShowAll] = useState(true);
+  const [errorMessage, setErrorMessage] = useState(null);
 
   useEffect(() => {
     noteService.getAll().then((initialNotes) => {
@@ -30,7 +29,6 @@ const App = () => {
   };
 
   const toggleImportanceOf = (id) => {
-    const url = `http://localhost:3001/notes/${id}`;
     const note = notes.find((n) => n.id === id);
     const changedNote = { ...note, important: !note.important };
 
@@ -46,7 +44,6 @@ const App = () => {
         setTimeout(() => {
           setErrorMessage(null);
         }, 5000);
-        setNotes(notes.filter((n) => n.id !== id));
       });
   };
 
--- part2/class_materials/2e_adding_styles/src/components/Notification.jsx
+++ part2/class_materials/2e_adding_styles/src/components/Notification.jsx
@@ -5,4 +5,5 @@ const Notification = ({ message }) => {
 
   return <div className="error">{message}</div>;
 };
+
 export default Notification;
