dc9bc8ad706ec740f58b0a5a93eec9cf7306d89b
odin <git config --global user.email 123456789+odin@users.noreply.github.com>
2024-11-02 01:14:57 +0400

    Fixed names for exact match. Show buttons added

--- part2/excercises/countries/src/App.jsx
+++ part2/excercises/countries/src/App.jsx
@@ -2,6 +2,16 @@ import axios from 'axios';
 import { useState, useEffect } from 'react';
 import Country from './components/Country';
 
+const ListedCountry = ({ country, handleShow }) => {
+  // console.log(country);
+  return (
+    <li>
+      {country.name.common}
+      <button onClick={() => handleShow(country)}>show</button>
+    </li>
+  );
+};
+
 function App() {
   const [country, setCountry] = useState({});
   const [inputCountry, setInputCountry] = useState('');
@@ -15,58 +25,75 @@ function App() {
       .then((data) => {
         setCountriesDatabase(data);
       });
-
-    countriesDatabase.forEach((element) => {
-      // console.log('==================================');
-      // console.log('element.name', element.name.common);
-      // console.log('==================================');
-    });
   }, []);
 
   const searchCountry = (event) => {
     event.preventDefault();
 
-    const searchedCountries = countriesDatabase.filter((country) =>
-      country.name.common.toLowerCase().includes(inputCountry.toLowerCase())
-    );
-    console.log('==================================');
-    console.log('searchedCountries', searchedCountries);
-    console.log('==================================');
+    let matchingCountries = countriesDatabase.filter((country) => {
+      const countryName = country.name.common.toLowerCase();
+      const searchInput = inputCountry.toLowerCase();
+      return countryName.includes(searchInput);
+    });
 
-    if (searchedCountries.length === 1) {
+    if (matchingCountries.length === 1) {
       setResults([]);
-      setCountry(searchedCountries[0]);
-    } else if (searchedCountries.length <= 10) {
-      setResults(searchedCountries);
+      setCountry(matchingCountries[0]);
+    } else if (matchingCountries.length === 0) {
       setCountry([]);
-      console.log('<=10');
-    } else {
       setResults([]);
+      console.log(0);
+    } else if (matchingCountries.length <= 10) {
+      // Check if exact match is in array
+      for (let i = 0; i < matchingCountries.length; i++) {
+        const country = matchingCountries[i];
+        if (country.name.common.toLowerCase() === inputCountry.toLowerCase()) {
+          console.log('exact match');
+          matchingCountries = [country];
+          setCountry(matchingCountries[0]);
+          setResults([]);
+          break;
+        } else if (matchingCountries.length === i + 1) {
+          console.log('got to end');
+          setResults(matchingCountries);
+          setCountry([]);
+          console.log('<=10');
+        }
+      }
+    } else {
+      setCountry([]);
+      setResults([
+        { name: { common: 'Too many matches, specify another filter' } },
+      ]);
       console.log('> 10');
     }
+  };
+
+  const handleShowButton = (country) => {
+    console.log(country);
+    setCountry(country);
+    setResults([]);
   };
 
   const handleInputChange = (event) => {
     setInputCountry(event.target.value);
   };
-
-  // if (Object.keys(country).length === 0) {
-  //   console.log('no country');
-  // } else {
-  //   console.log('country exists');
-  // }
   return (
     <>
       <form onSubmit={searchCountry}>
         <input type="text" value={inputCountry} onChange={handleInputChange} />
         <button type="submit">Search</button>
       </form>
-      <Country countryObject={country} />
       <ul>
-        {results.map((country, i) => (
-          <li key={i}>{country.name.common}</li>
+        {results.map((country) => (
+          <ListedCountry
+            country={country}
+            handleShow={() => handleShowButton(country)}
+            key={country.cca2}
+          />
         ))}
       </ul>
+      <Country countryObject={country} />
     </>
   );
 }
--- part2/excercises/countries/src/components/Country.jsx
+++ part2/excercises/countries/src/components/Country.jsx
@@ -1,32 +1,42 @@
-const Country = ({ countryObject }) => {
-  console.log('==================================');
-  console.log('countryObject', countryObject);
-  console.log('==================================');
-  if (Object.keys(countryObject).length === 0) {
-    console.log('no keys');
-    console.log(countryObject);
+import axios from 'axios';
+import api from '../services/api';
 
+const Country = ({ countryObject }) => {
+  if (Object.keys(countryObject).length === 0) {
     return <></>;
   } else {
-    console.log('keys???????????');
+    // const capitalLatitude = countryObject.capitalInfo.latlng[0];
+    // const capitalLongitude = countryObject.capitalInfo.latlng[1];
+    // console.log('==================================');
+    // console.log('capitalLatitude', capitalLatitude);
+    // console.log('api', api);
+    // console.log('==================================');
+    // const weatherData = axios
+    //   .get(
+    //     `https://api.openweathermap.org/data/3.0/onecall?lat=${capitalLatitude}&lon=${capitalLongitude}&appid=${api}`
+    //   )
+    //   .then((response) => {
+    //     console.log(response);
+    //   });
+    // console.log(weatherData);
+
     console.log('==================================');
     console.log('countryObject', countryObject);
     console.log('==================================');
-
     return (
-      <>
+      <div key={countryObject.ccn3}>
         <h2>{countryObject.name.common}</h2>
         <h3></h3>
         <p>Capital {countryObject.capital[0]}</p>
         <p>Area {countryObject.area}</p>
         <h3>Languges:</h3>
         <ul>
-          {Object.entries(countryObject.languages).map(([key, value]) => (
-            <li> {value}</li>
+          {Object.entries(countryObject.languages).map(([key, value], i) => (
+            <li key={i}> {value}</li>
           ))}
         </ul>
         <img src={countryObject.flags.png} alt="flag" />
-      </>
+      </div>
     );
   }
 };
