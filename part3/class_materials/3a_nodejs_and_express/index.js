c1bbfb772bc0b9527232c5c478380f01817e5317
Levan Vachnadze <lvachnadze1@gmail.com>
2024-11-14 20:24:17 +0400

    Express routing

--- part3/class_materials/3a_nodejs_and_express/index.js
+++ part3/class_materials/3a_nodejs_and_express/index.js
@@ -1,25 +1,30 @@
-const http = require('http');
+const express = require("express");
+const app = express();
 
 let notes = [
   {
-    id: '1',
-    content: 'HTML is easy',
+    id: "1",
+    content: "HTML is easy",
     important: true,
   },
   {
-    id: '2',
-    content: 'Browser can execute only JavaScript',
+    id: "2",
+    content: "Browser can execute only JavaScript",
     important: false,
   },
   {
-    id: '3',
-    content: 'GET and POST are the most important methods of HTTP protocol',
+    id: "3",
+    content: "GET and POST are the most important methods of HTTP protocol",
     important: true,
   },
 ];
-const app = http.createServer((request, response) => {
-  response.writeHead(200, { 'Content-Type': 'application/json' });
-  response.end(JSON.stringify(notes));
+
+app.get("/", (request, response) => {
+  response.send("<h1>Hello World! </h1>");
+});
+
+app.get("/api/notes", (request, response) => {
+  response.json(notes);
 });
 
 const PORT = 3001;
