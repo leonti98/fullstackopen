5214bedea73d3fe69a01cc04946599039132e62e
Levan Vachnadze <lvachnadze1@gmail.com>
2024-11-25 18:28:21 +0400

    Created directory 3c part

---
+++ part3/class_materials/3c_saving_data_to_MongoDB/backend/mongo.js
@@ -0,0 +1,33 @@
+const mongoose = require("mongoose");
+
+if (process.argv.length < 3) {
+  console.log(
+    "Please provide the password as an argument: node mongo.js <password>"
+  );
+  process.exit(1);
+}
+
+const password = process.argv[2];
+
+const url = `mongodb+srv://lvachnadze:${password}@cluster0.btchr.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+mongoose.set("strictQuery", false);
+
+mongoose.connect(url);
+
+const noteSchema = new mongoose.Schema({
+  content: String,
+  important: Boolean,
+});
+
+const Note = mongoose.model("Note", noteSchema);
+
+const note = new Note({
+  content: "HTML is Easy",
+  important: true,
+});
+
+note.save().then((result) => {
+  console.log("note saved!");
+  mongoose.connection.close();
+});
