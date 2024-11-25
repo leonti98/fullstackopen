25960b68b87d8761d67a91afc5a18495e540060a
odin <git config --global user.email 123456789+odin@users.noreply.github.com>
2024-11-26 02:22:29 +0400

    Fetching objects from the database

--- part3/class_materials/3c_saving_data_to_MongoDB/backend/mongo.js
+++ part3/class_materials/3c_saving_data_to_MongoDB/backend/mongo.js
@@ -1,17 +1,17 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
 
 if (process.argv.length < 3) {
   console.log(
-    "Please provide the password as an argument: node mongo.js <password>"
+    'Please provide the password as an argument: node mongo.js <password>'
   );
   process.exit(1);
 }
 
 const password = process.argv[2];
 
-const url = `mongodb+srv://lvachnadze:${password}@cluster0.btchr.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+const url = `mongodb+srv://lvachnadze:${password}@cluster0.btchr.mongodb.net/noteApp?retryWrites=true&w=majority&appName=Cluster0`;
 
-mongoose.set("strictQuery", false);
+mongoose.set('strictQuery', false);
 
 mongoose.connect(url);
 
@@ -20,14 +20,21 @@ const noteSchema = new mongoose.Schema({
   important: Boolean,
 });
 
-const Note = mongoose.model("Note", noteSchema);
+const Note = mongoose.model('Note', noteSchema);
 
-const note = new Note({
-  content: "HTML is Easy",
-  important: true,
-});
+// const note = new Note({
+//   content: 'HTML is Easy',
+//   important: true,
+// });
 
-note.save().then((result) => {
-  console.log("note saved!");
+// note.save().then((result) => {
+//   console.log('note saved!');
+//   mongoose.connection.close();
+// });
+
+Note.find({}).then((result) => {
+  result.forEach((note) => {
+    console.log(note);
+  });
   mongoose.connection.close();
 });
