fa14e2aed8032fb48f689d0b1fbba9eb3d692cf8
odin <git config --global user.email 123456789+odin@users.noreply.github.com>
2024-11-28 03:06:26 +0400

    Copied version from phonebook repo

---
+++ part3/excercises/phonebook/models/phonebook.js
@@ -0,0 +1,40 @@
+const mongoose = require('mongoose');
+
+mongoose.set('strictQuery', false);
+
+const url = process.env.MONGODB_URI;
+
+console.log('connecting to', url);
+
+mongoose
+  .connect(url)
+  .then(() => console.log('connected to MongoDB'))
+  .catch((error) => console.log('error connecting to MongoDB:', error.message));
+
+const phonebookSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    minlength: 3,
+  },
+  number: {
+    type: String,
+    minlength: 8,
+    validate: {
+      validator: function (v) {
+        return /^\d{2,3}-\d+$/.test(v);
+      },
+      message: (props) => `${props.value} is not a valid phone number!`,
+    },
+  },
+});
+
+phonebookSchema.set('toJSON', {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString();
+    delete returnedObject._id;
+    delete returnedObject.__v;
+  },
+});
+
+module.exports = mongoose.model('Phonebook', phonebookSchema);
