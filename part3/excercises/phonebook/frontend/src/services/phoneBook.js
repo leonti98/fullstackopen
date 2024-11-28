71296a4b3d56ceeb939f21a1c28bfb2b1c383efe
odin <git config --global user.email 123456789+odin@users.noreply.github.com>
2024-11-28 18:26:52 +0400

    Updated to latest version

--- part3/excercises/phonebook/frontend/src/services/phoneBook.js
+++ part3/excercises/phonebook/frontend/src/services/phoneBook.js
@@ -1,28 +1,28 @@
-import axios from "axios";
+import axios from 'axios'
 
-const baseURL = "http://localhost:3001/api/persons";
+const baseURL = '/api/persons'
 
 const getPeople = () => {
-  const request = axios.get(baseURL);
+  const request = axios.get(baseURL)
   // axios.get(baseURL).then((response) => response.data);
-  return request.then((response) => response.data);
-};
+  return request.then((response) => response.data)
+}
 
 const addPerson = (newEntry) => {
-  const request = axios.post(baseURL, newEntry);
-  return request.then((response) => response.data);
-};
+  const request = axios.post(baseURL, newEntry)
+  return request.then((response) => response.data)
+}
 
 const deletePerson = (id) => {
-  const request = axios.delete(`${baseURL}/${id}`);
-  return request.then((response) => response.data);
-};
+  const request = axios.delete(`${baseURL}/${id}`)
+  return request.then((response) => response.data)
+}
 
 const updatePersonNumber = (id, newEntry) => {
-  console.log(id, newEntry);
+  console.log(id, newEntry)
 
-  const request = axios.put(`${baseURL}/${id}`, newEntry);
-  return request.then((response) => response.data);
-};
+  const request = axios.put(`${baseURL}/${id}`, newEntry)
+  return request.then((response) => response.data)
+}
 
-export default { getPeople, addPerson, deletePerson, updatePersonNumber };
+export default { getPeople, addPerson, deletePerson, updatePersonNumber }
