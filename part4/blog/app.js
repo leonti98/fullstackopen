87850806815ddf9eacd2e4ee08ddf7ffe66d0c43
Arch <lvachnadze1@gmail.com>
2024-12-09 00:42:58 +0400

    syntax

--- part4/blog/app.js
+++ part4/blog/app.js
@@ -1,23 +1,23 @@
-const config = require('./utils/config');
-const express = require('express');
+const config = require("./utils/config");
+const express = require("express");
 const app = express();
-const cors = require('cors');
-const blogRouter = require('./controllers/blog');
-const middleware = require('./utils/middleware');
-const { error, info } = require('./utils/logger');
-const mongoose = require('mongoose');
+const cors = require("cors");
+const blogRouter = require("./controllers/blog");
+const middleware = require("./utils/middleware");
+const logger = require("./utils/logger");
+const mongoose = require("mongoose");
 
 const mongoUrl = config.MONGODB_URI;
-info('connecting to database');
+logger.info("connecting to database");
 mongoose.connect(mongoUrl).catch((error) => {
-  error('error connecting to MongoDB:', error.message);
+  logger.error("error connecting to MongoDB:", error.message);
 });
 
 app.use(cors());
 app.use(express.json());
 app.use(middleware.requestLogger);
 
-app.use('/api/blogs', blogRouter);
+app.use("/api/blogs", blogRouter);
 
 app.use(middleware.unknownEndpoint);
 app.use(middleware.errorHandler);
--- part4/notes/mongo.js
+++ part4/notes/mongo.js
@@ -1,22 +1,25 @@
-const mongoose = require('mongoose');
+const config = require("./utils/config");
+const logger = require("./utils/logger");
+const mongoose = require("mongoose");
 
 if (process.argv.length < 3) {
-  console.log('give password as argument');
-  process.exit(1);
+  console.log("give password as argument");
+  process.exzit(1);
 }
 
 const password = process.argv[2];
 
-const url = `mongodb+srv://lvachnadze:${password}@cluster0.btchr.mongodb.net/noteApp?retryWrites=true&w=majority&appName=Cluster0`;
+const url = config.MONGODB_URI;
 
-mongoose.set('strictQuery', false);
+logger.info("connecting to database");
+mongoose.set("strictQuery", false);
 mongoose.connect(url).then(() => {
   const noteSchema = new mongoose.Schema({
     content: String,
     important: Boolean,
   });
 
-  const Note = mongoose.model('Note', noteSchema);
+  const Note = mongoose.model("Note", noteSchema);
 
   /*
   const note = new Note({
