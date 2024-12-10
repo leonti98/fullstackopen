88b572687b06ca2cb2096be3acda80f91989e0fa
odin <git config --global user.email 123456789+odin@users.noreply.github.com>
2024-12-10 11:51:02 +0400

    remove quote requirement

--- part4/notes/eslint.config.mjs
+++ part4/notes/eslint.config.mjs
@@ -19,7 +19,7 @@ export default [
     rules: {
       '@stylistic/js/indent': ['error', 2],
       '@stylistic/js/linebreak-style': ['error', 'unix'],
-      '@stylistic/js/quotes': ['error', 'single'],
+      // '@stylistic/js/quotes': ['error', 'single'],
       '@stylistic/js/semi': 'off',
       eqeqeq: 'error',
       'no-trailing-spaces': 'error',
